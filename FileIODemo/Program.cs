954e6ebf494785ea15a2f70508c9506b001b4035
vundelayamini <vundelayamini@gmail.com>
2021-05-10 20:05:17 +0530

    [yamini]Add: Welcome message to FileIO demo

---
+++ FileIODemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome message to FileIO demo");
+        }
+    }
+}
